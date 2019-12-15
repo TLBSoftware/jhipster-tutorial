670e25c4aadc2d55ae06d19ffbee2eb1bd647b6c
Taylor Bryant <tnayrbt40@gmail.com>
2019-12-15 15:03:00 -0500

    Generated jhipster application

---
+++ src/main/java/com/tlbsoftware/bugtracker/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.tlbsoftware.bugtracker.config.audit;
---
+++ src/main/java/com/tlbsoftware/bugtracker/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package com.tlbsoftware.bugtracker.service.dto;
